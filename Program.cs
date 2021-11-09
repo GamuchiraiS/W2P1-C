3884ef4f1154a3d267de76ace3eb8c25b0f622ce
GamuchiraiS <80484981+GamuchiraiS@users.noreply.github.com>
2021-11-09 17:36:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for printing numbers from 0 - 6
+            for (int n = 0; n < 6; n++)
+            {
+                //prevents the no. 3 from being printed
+                if (n == 3)
+                {
+                    continue; //breaks one iteration and continues with next iteration
+                }
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
